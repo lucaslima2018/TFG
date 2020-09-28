4b5d7156e1d39c4891e18e759d7dd795367faa4d
lucaslima2018 <lucas.lima@ufn.edu.br>
2020-09-28 18:12:11 -0300

    Abrir Arquivos de Áudio em Python

--- src2/abrindo_arquivo_de_audio.py
+++ src2/abrindo_arquivo_de_audio.py
@@ -3,35 +3,57 @@ import numpy as np
 import wave
 import struct
 import math
-
-
 def main(args):
-    arquivoWav = wave.open('../../../Desktop/tri.wav', 'r')
-    print("Número canais: ", arquivoWav.getnchannels())
+    '''Abrir o arquivo “exemplo1.wav”, com o comando open do pacote wave no modo leitura'''
+    arquivoWave = wave.open('exemplo1.wav', 'r')
 
-    print("Número bytes: ", arquivoWav.getsampwidth())
+    '''A função “getnchannels()” retorna o número de canais armazenados no arquivo wav. No exemplo
+    somente arquivos mono serão processados'''
+    print("Número canais: ", arquivoWave.getnchannels())
 
-    print("Taxa de amostragem: ", arquivoWav.getframerate())
+    '''A função “getsamplewidth()” retorna o numero de bytes por amostra. No exemplo apenas 2 bytes por
+    amostra'''
+    print("Número bytes: ", arquivoWave.getsampwidth())
 
-    print("Número de frames: ", arquivoWav.getnframes())
+    '''A função “getframerate()” retorna a taxa de amostragem (amostras por segundo) com que foram 
+    gravados os dados wav.'''
+    print("Taxa de amostragem: ", arquivoWave.getframerate())
 
-    print("Compactação: ", arquivoWav.getcompname())
+    '''A função “getnframes” retorna o número de frames, ou seja, a quantidade de dados armazenados.'''
+    print("Número de frames: ", arquivoWave.getnframes())
 
-    frames = arquivoWav.readframes(arquivoWav.getnframes())
+    '''A função “getcompname()” retorna o tipo de compactação. Não utilizamos nenhuma compactação em 
+    arquivos wav.'''
+    print("Compactação: ", arquivoWave.getcompname())
 
-    deltaX = 1.0 / arquivoWav.getframerate()
+    '''A função “readFrames(nFrames)” retorna uma string binária com todos os bytes armazenados na 
+    área de dados do arquivo wav.'''
+    frames = arquivoWave.readframes(arquivoWave.getnframes())
 
-    tempo = np.arange(start=0, stop=arquivoWav.getnframes() * deltaX, step=deltaX, dtype=np.float)
+    '''DeltaX é o intervalo de amostragem, em segundos.'''
+    deltaX = 1.0 / arquivoWave.getframerate()
 
-    wavDataList = [struct.unpack("<h", frames[nLoop] + frames[nLoop + 1])[0] for nLoop in range(0, len(frames), 2)]
+    '''Criar array numpy que conterá os pontos de amostragem distribuídos no tempo.'''
+    tempo = np.arange(start=0, stop=arquivoWave.getnframes() * deltaX, step=deltaX, dtype=np.float)
 
-    wavArray = np.array(wavDataList)
+    '''Converter string binária lida através de “readframes” para uma lista python.'''
+    waveDataList = [struct.unpack("<h", frames[nLoop] + frames[nLoop + 1])[0] for nLoop in range(0, len(frames), 2)]
 
-    plt.plot(tempo[0:100], wavArray[0:100])
+    '''Converter a lista python para um array numpy, mais adequado para processamento matemático.'''
+    waveArray = np.array(waveDataList)
+
+    '''Preparar para traçar o gráfico dos primeiros 100 pontos do array. 100 pontos foram selecionados 
+    para apresentar cerca de dois ciclos completos na tela.'''
+    plt.plot(tempo[0:100], waveArray[0:100])
+
+    '''Inserir grade na tela'''
     plt.grid()
+
+    '''Mostrar o gráfico. Para fechar pressione o “x” no canto superior direito.'''
     plt.show()
 
-    arquivoWav.close()
+    '''Fechar arquivo wav.'''
+    arquivoWave.close()
     return 0
 
 
